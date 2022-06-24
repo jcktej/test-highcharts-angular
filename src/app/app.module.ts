28dcf0ae73436363752a6362ce05274de790671d
Jorge Tejada <jcktej@gmail.com>
2022-06-24 16:47:38 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { HighchartsChartModule } from "highcharts-angular";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HighchartsChartModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
